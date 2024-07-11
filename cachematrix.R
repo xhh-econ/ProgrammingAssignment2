2203ff6f6f7541f377fba1c9ff0941f7866608f1
xhh-econ <82673644+xhh-econ@users.noreply.github.com>
2024-07-10 17:07:37 -0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below two functions are used to create a special object that stores 
+## a matrix and cache's its inverse matrix.
 
-## Write a short comment describing this function
+## The first function makeCacheMatrix creates a special "Vector", a list 
+## that contains four elements:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse matrix
+## 4. get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix from 
+## the special "vector" created above. It checks to see if the 
+## inverse matrix has been calculated. If so, it gets the inverse 
+## matrix from the cache and skips the computation. Otherwise, it 
+## calculates the inverse of the matrix and sets the value of the 
+## matrix in the cache via the sematrix function. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!all(is.na(m))) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
+
 }
